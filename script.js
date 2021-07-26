03fdb52b736a4c6d34894030edf44e196ca9ccd2
Mahidhar Mannuru <81514511+MahidharMannuru5@users.noreply.github.com>
2021-07-26 12:17:58 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+function nextOne(){
+    let randomNumber3=Math.floor(Math.random()*2)+1;
+        document.querySelector(".img").setAttribute("src","mevideos/video"   +randomNumber3+".webm")
+        
+    }
